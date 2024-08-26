32adadd8210721f9983ea2866acc7d35134f0f71
Kiel Pykett <pykettk@gmail.com>
2024-08-26 15:05:58 +0100

    Ensure OpCache Enabled Before Adding Flush Button

--- Block/Adminhtml/OpCache/Flush.php
+++ Block/Adminhtml/OpCache/Flush.php
@@ -7,15 +7,27 @@ declare(strict_types=1);
 
 namespace Element119\AdminOpCacheReport\Block\Adminhtml\OpCache;
 
+use Element119\AdminOpCacheReport\ViewModel\OpCache;
 use Magento\Backend\Block\Widget\Container;
+use Magento\Backend\Block\Widget\Context;
 
 class Flush extends Container
 {
+    public function __construct(
+        private readonly Context $context,
+        private readonly OpCache $opCache,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
     protected function _construct()
     {
         parent::_construct();
 
-        if ($this->_authorization->isAllowed('Element119_AdminOpCacheReport::flush')) {
+        if ($this->opCache->getOpCacheStatusData('opcache_enabled')
+            && $this->_authorization->isAllowed('Element119_AdminOpCacheReport::flush')
+        ) {
             $message = __('Caution: Flushing the PHP OpCache will lead to temporary performance degradation. See the <a href="https://www.php.net/manual/en/intro.opcache.php" target="_blank">PHP OpCache documentation</a> for more information.');
             $this->buttonList->add(
                 'flush_opcache',
